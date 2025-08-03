8cbdb7af283fe32d591d8cacf012627f32cc9ff0
Salman  N <salmanneeya7@gmail.com>
2025-08-03 17:04:48 +0200

    Update and rename script.sql to Analyse des retards de vols.sql

---
+++ Analyse des retards de vols.sql
@@ -0,0 +1,7 @@
+/*
+Le projet utilise un fichier de données (DelayedFlights.csv) pour étudier les causes des retards, identifier les compagnies et aéroports les plus affectés, suivre l’évolution des retards dans le temps, et établir un classement des compagnies aériennes selon leur ponctualité. 
+L’analyse s’appuie sur des requêtes SQL pour extraire des indicateurs clés et faciliter la prise de décision dans le secteur aérien.
+Source : [https://www.kaggle.com/datasets/giovamata/airlinedelaycauses)](https://www.kaggle.com/datasets/giovamata/airlinedelaycauses
+*/
+
+
